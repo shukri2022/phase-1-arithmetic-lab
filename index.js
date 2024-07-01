7f7c74217b6f518ba6240a5626d9e20b418007a0
Shukri Abdi <shukri751@gmail.com>
2024-07-01 21:29:45 +0300

    Added changes

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+const num3 = 28;
+const num4 = 6;
+
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(1, 2, 3, 4, 5, 20, 10, 15);
+console.log(max); // 20
